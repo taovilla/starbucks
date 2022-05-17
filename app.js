253d068f7a2bd05a70340b29355dd8065d4af8b5
taofeek <olaopataofeek@gmail.com>
2022-05-17 17:48:24 +0800

    starbucks

---
+++ app.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToogle(){
+    btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToogle)
+
+
+
+
+
+
